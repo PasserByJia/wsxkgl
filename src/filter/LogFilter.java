1fcedeead56b62bddf3422810f5a22e828ee3163
jh <jh_personal@163.com>
2018-04-26 16:49:24 +0800

    add LogFilter

---
+++ src/filter/LogFilter.java
@@ -0,0 +1,33 @@
+package filter;
+
+import cn.edu.sdjzu.xg16.bysj.domain.User;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@WebFilter(filterName = "MessageFilter",urlPatterns = {"/*"})
+public class LogFilter implements Filter {
+    @Override
+    public void destroy(){}
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        String loginTime= new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date());
+        HttpServletRequest request = (HttpServletRequest)req;
+        User user = (User)request.getSession().getAttribute("user");
+        if(user == null){
+            request.getRequestDispatcher("/login/loginController.do").forward(request,resp);
+        }
+        else {
+            String name = user.getTeacher().getName();
+            String uri = request.getRequestURI();
+            System.out.println(loginTime + " ," +name+ ","+uri+",");
+            chain.doFilter(request,resp);
+        }
+    }
+}
+
