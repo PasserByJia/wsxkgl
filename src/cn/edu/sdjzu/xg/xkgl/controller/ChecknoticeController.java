3798352d820d8e3b95e2c7a65f6ce4457b4e4969
xiao <1697745921@qq.com>
2018-04-29 10:49:23 +0800

    删除测试的注释

--- src/cn/edu/sdjzu/xg/xkgl/controller/ChecknoticeController.java
+++ src/cn/edu/sdjzu/xg/xkgl/controller/ChecknoticeController.java
@@ -15,19 +15,14 @@ import java.util.TreeSet;
 
 @WebServlet("/checknoticeController")
 public class ChecknoticeController extends HttpServlet{
-    //查看通知详情
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
        int id = Integer.parseInt(req.getParameter("id"));
        System.out.println(id);
         try {
-            //通过id查找通知
             Notice notice = NoticeService.getInstance().find(id);
-            //将找到的notice存入请求
             req.setAttribute("notice",notice);
-            //转发页面
             req.getRequestDispatcher("pages/detailedNotice.jsp").forward(req,resp);
         } catch (SQLException e) {
-            //错误转发
             req.setAttribute("message","查看通知详情失败");
             req.getRequestDispatcher("/pages/error.jsp").forward(req,resp);
         }
